d715afa472f8f09597ff198e9e7a50134504ad8b
Kuugal <Kuugal@live.com>
2019-03-11 20:11:58 -0600

    A,B,C,D-V1

--- src/components/DLabMic.vue
+++ src/components/DLabMic.vue
@@ -1,5 +1,8 @@
 <template>
   <v-app id="CoordinacionI">
+   
+<!-- START NAV-SIDEBAR -->
+   
     <v-navigation-drawer
       v-model="drawer"
       fixed
@@ -7,7 +10,6 @@
       class="grey lighten-4"
       app
     >
-
 <v-list>
       <v-list-tile>
         <v-list-tile-action>
@@ -100,7 +102,7 @@
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Dirección de Mecatrónica</v-list-tile-title>
+        <router-link to="/b-dir-ni"><v-list-tile-title>Dirección de Negocios Internacionales</v-list-tile-title></router-link>
       </v-list-tile>
       
 
@@ -108,21 +110,21 @@
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Laboratorio C</v-list-tile-title>
+        <router-link to="/b-lab-c"><v-list-tile-title>Laboratorios de computo</v-list-tile-title></router-link>
       </v-list-tile>
 
       <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Salón de actividades</v-list-tile-title>
+        <router-link to="/b-dir-b"><v-list-tile-title>Dirección de biotecnología</v-list-tile-title></router-link>
       </v-list-tile>
 
       <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Auditorio B</v-list-tile-title>
+        <router-link to="/b-aud-b"><v-list-tile-title>Auditorio B</v-list-tile-title></router-link>
       </v-list-tile>
        
     </v-list-group>
@@ -138,7 +140,7 @@
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Cafetería</v-list-tile-title>
+        <router-link to="/c-caf"><v-list-tile-title>Cafetería</v-list-tile-title></router-link>
       </v-list-tile>
        
     </v-list-group>
@@ -245,111 +247,71 @@
       </v-list-tile>
        
     </v-list-group>
+
     <v-list-group prepend-icon="account_balance">
 <v-list-tile slot="activator">
-          <v-list-tile-title>Edificio E*</v-list-tile-title>
+          <v-list-tile-title>Edificio E</v-list-tile-title>
         </v-list-tile>
 
          <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Control escolar</v-list-tile-title>
+         <router-link to="/e-con"><v-list-tile-title>Control escolar</v-list-tile-title></router-link>
       </v-list-tile>
 
        <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Servicio médico</v-list-tile-title>
+         <router-link to="/e-ser"><v-list-tile-title>Servicio médico</v-list-tile-title></router-link>
       </v-list-tile>
 
        <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Finanzas</v-list-tile-title>
+         <router-link to="/e-fin"><v-list-tile-title>Finanzas</v-list-tile-title></router-link>
       </v-list-tile>
 
        <v-list-tile>
         <v-list-tile-action>
           <v-icon>all_out</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Incubadora de proyectos</v-list-tile-title>
+         <router-link to="/e-inc"><v-list-tile-title>Incubadora de proyectos</v-list-tile-title></router-link>
       </v-list-tile>
 
     </v-list-group>
-
-    <v-list-group prepend-icon="account_balance">
-<v-list-tile slot="activator">
-          <v-list-tile-title>Edificio F***</v-list-tile-title>
-        </v-list-tile>
-    </v-list-group>
-
-    <v-list-group
-        prepend-icon="account_balance"
-        value="true"
-      >
-<v-list-tile slot="activator">
-          <v-list-tile-title>Edificio G***</v-list-tile-title>
-        </v-list-tile>
-       
-            <v-list-tile>
+    
+    <v-list-tile>
         <v-list-tile-action>
-          <v-icon>all_out</v-icon>
+          <v-icon>account_balance</v-icon>
         </v-list-tile-action>
-         <router-link to="/coordinacion-i"><v-list-tile-title>Coordinación de inglés</v-list-tile-title></router-link>
-      </v-list-tile>
-      
-
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon>all_out</v-icon>
-        </v-list-tile-action>
-         <router-link to="/dir-er"><v-list-tile-title>Dirección de energías renovables</v-list-tile-title></router-link>
-      </v-list-tile>
-       
-    </v-list-group>
-     <v-list-group
-        prepend-icon="account_balance"
-        
-      >
-<v-list-tile slot="activator">
-          <v-list-tile-title>Edificio H</v-list-tile-title>
-        </v-list-tile>
-       
-       
-
-<v-list-tile>
-        <v-list-tile-action>
-          <v-icon>all_out</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>Laboratorio camión</v-list-tile-title>
-      </v-list-tile>
-
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon >all_out</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>Laboratorios no usados</v-list-tile-title>
+        <router-link to="/f-sal"><v-list-tile-title>Edificio F</v-list-tile-title></router-link>
       </v-list-tile>
 
        <v-list-tile>
         <v-list-tile-action>
-          <v-icon >all_out</v-icon>
+          <v-icon>account_balance</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Laboratorios no usados</v-list-tile-title>
+        <router-link to="/g-can"><v-list-tile-title>Edificio G</v-list-tile-title></router-link>
       </v-list-tile>
 
-       <v-list-tile>
+ <v-list-tile>
         <v-list-tile-action>
-          <v-icon >all_out</v-icon>
+          <v-icon>account_balance</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>Bodegas</v-list-tile-title>
-      </v-list-tile>      
-       
-    </v-list-group>
+        <router-link to="/h-lab"><v-list-tile-title>Edificio H</v-list-tile-title></router-link>
+      </v-list-tile>
+    
+     <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>account_balance</v-icon>
+        </v-list-tile-action>
+        <router-link to="/i-sal"><v-list-tile-title>Edificio I</v-list-tile-title></router-link>
+      </v-list-tile>
 
+<!--
  <v-list-group prepend-icon="account_balance">
 <v-list-tile slot="activator">
           <v-list-tile-title>Casetas de vigilancia</v-list-tile-title>
@@ -369,17 +331,21 @@
         <v-list-tile-title>Entrada de vehiculos</v-list-tile-title>
       </v-list-tile>  
 
-    </v-list-group>
+    </v-list-group>-->
 
 
 </v-list>
 
-    </v-navigation-drawer>
-    <v-toolbar color="green" app absolute clipped-left class="white--text">
+</v-navigation-drawer>
+
+<v-toolbar color="green" app absolute clipped-left class="white--text">
       <v-toolbar-side-icon @click="drawer = !drawer" class="white--text"></v-toolbar-side-icon>
       <span class="title ml-3 mr-5 ">¿A dónde quieres ir?&nbsp;<span class="font-weight-light">UPVT</span></span>      
       <v-spacer></v-spacer>
     </v-toolbar>
+
+<!-- END NAV-SIDEBAR -->
+
     <v-content>
       <v-container fluid fill-height class="grey lighten-4">
         <v-layout align-center justify-center column fill-height>
@@ -387,9 +353,9 @@
         <!--<v-card dark color="orange">
           <v-card-text class="px-0">12</v-card-text>
         </v-card>-->
-      <img alt="Vue logo" src='@/assets/EdificioG.png' class="flex xs4 offset-xs4">        
+      <img alt="Vue logo" src='@/assets/edificio-d.jpeg' class="flex xs12 offset-xs3">        
       <br>
-<h3>Laboratorio de microbiología</h3>
+<h3>Laboratorio de cómputo</h3>
 <br>
 <img alt="Vue logo" src='@/assets/microbiologia.png' class="flex xs12">        
 <br>
@@ -398,26 +364,21 @@
 
       <v-flex xs12>
         <br>
- <strong>Objetivo General</strong>
+ 
  <p>
-El objetivo general de la enseñanza del inglés es que los estudiantes obtengan los conocimientos necesarios para participar en prácticas sociales del lenguaje de manera oral y escrita, con hablantes nativos y no nativos del inglés, a través de actividades que conllevan la producción e interpretación de diversos textos escritos y orales de naturaleza cotidiana, académica y literaria.
+ Es el lugar habilitado para manejar estudiar y analizar microorganismos con la finalidad de extremas precauciones para evitar resultados erróneos.
 </p>
-<strong>Objetivos Específicos</strong>
+<strong>Horario de atención</strong>
 <p>
-    Sensibilizar a los estudiantes sobre la existencia de una lengua distinta de la materna, y se familiaricen con ella.
-    Que los estudiantes obtengan los conocimientos necesarios para comprender y utilizar la lengua inglesa de manera que puedan reconocer, entender y emplear expresiones ampliamente utilizadas.
-    Comprender y usar el inglés para desarrollar tareas comunicativas simples y cotidianas sobre temas conocidos o habituales relacionados con los ambientes familiar y comunitario, académico y de formación, o el literario y lúdico.
-    Que los estudiantes consoliden su dominio del inglés en situaciones comunicativas básicas y sean capaces de participar en actividades con el lenguaje, propias de prácticas sociales del lenguaje.
+   7:00 a 21:00
 </p>
-<strong>Misión</strong>
-<p>Proporcionar a los alumnos de las condiciones que les permitan formarse a través de una educación integral de calidad, incluyente, pertinente e innovadora para el desarrollo y fortalecimiento de las competencias afectivas, sociales y cognoscitivas que favorezcan las funciones mentales de razonamiento lógico, reflexión, integración de juicios, asi como el desarrollo de su creatividad.
-
-Establecer las estructuras dinámicas que propicien un ambiente democrático, que permita a los alumnos cultivar una actitud que responda a la práctica de los valores y al desenvolvimiento de sus capacidades, aprovechar conscientemente su potencial e interactuar productivamente con su entorno.</p>
-
-<strong>Visión</strong>
-<p>Ofrecer una educación de calidad que permita a los alumnos desarrollar las competencias y adquirir los aprendizajes esperados establecidos en el Plan de Estudios para Educación Básica 2011.
-
-Desarrollar Las Actividades didácticas centradas en el alumno y su aprendizaje, así como formar ciudadanos que aprecien y practiquen los valores para convivir armónicamente en sociedad.</p>
+<strong>Responsable</strong>
+<p>
+  ING. JUAN GARCÍA CONTRERAS
+</p>
+<p>
+ING. JOSÉ ANACLETO DE JESÚS
+</p>
         </v-flex>
         </v-layout>
         <v-layout row>
@@ -425,6 +386,7 @@ Desarrollar Las Actividades didácticas centradas en el alumno y su aprendizaje,
         </v-layout>
       </v-container>
     </v-content>
+
   </v-app>
 </template>
 
